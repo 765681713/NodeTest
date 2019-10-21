c8db4ee8af6e58b8cfa326511cbdb3c5e12fdba4
BYDeXuanLv <765681713@qq.com>
2019-10-21 13:03:32 +0800

    ejs 模板

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
- 
+const router = new Router();
+const static = require('koa-static');
+const views = require('koa-views')
+
+app.use(static(__dirname + '/public'));
+app.use(views('views',{extension:'ejs'}));
+app.use(router.routes())
+  .use(router.allowedMethods());
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
 // response
 app.use(ctx => {
   ctx.body = 'Hello Koa';
   
-  
+  next();
 });
- 
+
+router.get('/', (ctx, next) => {
+  // ctx.router available
+  ctx.render(index);
+});
+
+
 app.listen(3000);
